a46fbb9ecdb50e65809e2cb1b71899277147addc
--unset <ilia.ryzhkov@dbax.co.uk>
2021-10-14 20:37:14 +0100

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,14 +1,26 @@
 // Step 1: Import React
 import * as React from 'react'
 import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { MDXRenderer} from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 
 // Step 2: Define your component
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>{data.mdx.frontmatter.date}</p>
+      <p> {data.mdx.frontmatter.date}</p>
+      <GatsbyImage
+      image={image}
+      alt={data.mdx.frontmatter.hero_image_alt}
+      />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       <MDXRenderer>
         {data.mdx.body}
       </MDXRenderer>
@@ -17,11 +29,20 @@ const BlogPost = ({ data }) => {
 }
 
 export const query = graphql`
-    query ($id: String) {
-      mdx(id: {eq: $id}) {
+    query($slug: String) {
+      mdx(slug: {eq: $slug}) {
+        body
         frontmatter {
-          date(formatString: "DD/MM/YYYY")
           title
+          date(formatString: "MMMM DD, YYYY")
+          hero_image_alt
+          hero_image_credit_link
+          hero_image_credit_text
+          hero_image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
         }
         body
       }
